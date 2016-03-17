898327cac4751adcb67d0177d823950ed5c397c4
kmcole2 <kmcole2@illinois.edu>
2016-03-16 19:59:24 -0500

    test rotate

---
+++ rotatetest/rotate.js
@@ -0,0 +1,16 @@
+var angle = 0;
+
+interact('#rotate-area').gesturable({
+  onmove: function (event) {
+    var arrow = document.getElementById('arrow');
+
+    angle += event.da;
+
+    arrow.style.webkitTransform =
+    arrow.style.transform =
+      'rotate(' + angle + 'deg)';
+
+    document.getElementById('angle-info').textContent =
+      angle.toFixed(2) + '°';
+  }
+});
